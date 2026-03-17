bc0ee56cc484dde9ba485300504bf07ef5b36205
Erwin <erwintan151@gmail.com>
2026-03-17 09:33:33 +0800

    Initial Commit

---
+++ BMD Installer/BMD_InstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMD_InstallerApp.swift
+//  BMD Installer
+//
+//  Created by Erwin on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BMD_InstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
